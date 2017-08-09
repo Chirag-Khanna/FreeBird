76ca4c1290e30bb8cd5af42e71e6ba90af0750f4
chirag-compro <chirag.comprotechnologies.com>
2017-08-09 07:09:23 +0530

    changes for production

--- server/server.js
+++ server/server.js
@@ -17,12 +17,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Point static path to dist
 if (app.get('env') === 'production') {
-	app.use(express.static(path.join(__dirname, 'client')));	
-	app.use(express.static(path.join(__dirname, 'dist/client')));
+	app.use(express.static(path.join(__dirname, '/../client')));	
+	app.use(express.static(path.join(__dirname, '/../dist/client')));
 }
 else{
-	app.use(express.static(path.join(__dirname, 'client')));
-	app.use(express.static(path.join(__dirname, 'dist/client')));	
+	app.use(express.static(path.join(__dirname, '/../client')));
+	app.use(express.static(path.join(__dirname, '/../dist/client')));	
 }
 
 
